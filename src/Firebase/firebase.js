2637fff85d29dbbbeda7500737809f5ff81e50a8
Jerald George <jeraldgeo94@gmail.com>
2018-08-31 23:28:22 +0530

    Create two projects in firebase copied id and keys in it create a separate folder

---
+++ src/Firebase/firebase.js
@@ -0,0 +1,33 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const proConfig = {
+  apiKey: "AIzaSyCERyzZq_L3fuNcBcUznSnENHIkerHTTCo",
+  authDomain: "reactfirebase-228fd.firebaseapp.com",
+  databaseURL: "https://reactfirebase-228fd.firebaseio.com",
+  projectId: "reactfirebase-228fd",
+  storageBucket: "reactfirebase-228fd.appspot.com",
+  messagingSenderId: "208916795434"
+};
+
+const devConfig = {
+  apiKey: "AIzaSyAijJyWFLvDZZZD3ddhi5Yxi6ltZ06jO5Y",
+  authDomain: "reactfirebasefordevelopement.firebaseapp.com",
+  databaseURL: "https://reactfirebasefordevelopement.firebaseio.com",
+  projectId: "reactfirebasefordevelopement",
+  storageBucket: "",
+  messagingSenderId: "788976141354"
+};
+
+
+const config = process.env.NODE_ENV === 'production'
+  ? proConfig
+  : devConfig;
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+const auth = firebase.auth();
+
+export { auth };
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,52 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import * as routes from '../constants/routes';
+import * as routes from "../constants/routes";
+import SignOutButton from './SignOut';
+import AuthUserContext from './AuthUserContext';
 
-const Navigation = () =>
-  <div>
-    <ul>
-      <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
-      <li><Link to={routes.LANDING}>Landing</Link></li>
-      <li><Link to={routes.HOME}>Home</Link></li>
-      <li><Link to={routes.ACCOUNT}>Account</Link></li>
-    </ul>
-  </div>
+const Navigation = ({ authUser }) =>
+ <AuthUserContext.Consumer>
+    { authUser
+       ? <NavigationAuth />
+       : <NavigationNonAuth />
+   }
+ </AuthUserContext.Consumer>
+
+const NavigationAuth = () =>
+  <ul>
+    <li><Link to={routes.LANDING}>Landing</Link></li>
+    <li><Link to={routes.HOME}>Home</Link></li>
+    <li><Link to={routes.ACCOUNT}>Account</Link></li>
+    <li><SignOutButton /></li>
+  </ul>
+
+  const NavigationNonAuth = () =>
+  <ul>
+    <li><Link to={routes.LANDING}>Landing</Link></li>
+    <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
+  </ul>
+
+    // <ul>
+    //   <li>
+    //     <Link to={routes.SIGN_IN}>Sign In</Link>
+    //   </li>
+    //   <li>
+    //     <Link to={routes.SIGN_UP}>Sign Up</Link>
+    //   </li>
+    //   <li>
+    //     <Link to={routes.LANDING}>Landing</Link>
+    //   </li>
+    //   <li>
+    //     <Link to={routes.HOME}>Home</Link>
+    //   </li>
+    //   <li>
+    //     <Link to={routes.ACCOUNT}>Account</Link>
+    //   </li>
+    //   <li>
+    //     <Link to={routes.PASSWORD_FORGET}>Pw-forget</Link>
+    //   </li>
+    //   <li><SignOutButton/></li>
+    // </ul>
 
 export default Navigation;

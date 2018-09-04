dd359f40b8838ee51c8769cddd9ea9d2edcc5b11
Jerald George <jeraldgeo94@gmail.com>
2018-09-04 19:42:06 +0530

    Error fixed on sign out and sign up page

--- src/Firebase/db.js
+++ src/Firebase/db.js
@@ -7,19 +7,3 @@ db.ref(`users/${id}`).set({
 });
 
 export const onceGetUsers = () => db.ref('users').once('value');
-
-
-// import { db } from './firebase';
-//
-// // User API
-//
-// export const doCreateUser = (id, username, email) =>
-//   db.ref(`users/${id}`).set({
-//     username,
-//     email,
-//   });
-//
-// export const onceGetUsers = () =>
-//   db.ref('users').once('value');
-//
-// // Other Entity APIs ...
